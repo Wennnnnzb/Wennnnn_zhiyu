27f831bfefb290b8c9fa86ec8268907ac5c8b9cb
Wennnnnzb <2594224665@qq.com>
2026-05-13 22:14:37 +0800

    Add files via upload

---
+++ busuanzi.js
@@ -0,0 +1,7 @@
+// 不蒜子统计脚本 v2.3 (本地化版本)
+(function() {
+    var script = document.createElement('script');
+    script.src = 'https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js';
+    script.async = true;
+    document.head.appendChild(script);
+})();
